9bbfc03664e726e35797725c7369ce0a0244e9f6
Matias Suez <matisuez@gmail.com>
2021-03-08 11:40:37 -0300

    First commit - Init back-end and front-end projects

---
+++ back-end/public/controllers/movil.php
@@ -0,0 +1,73 @@
+<?php
+include_once "model/movil.php";
+
+
+$app->get('/movil', function ($request, $response, $args) {
+    //$token = G::Autenticar($request, "ADMIN_VER");
+
+    $db = SQL::connect();
+    $model = new Movil();
+
+    $results = $model->get($db);
+    SQL::close($db);
+
+    $payload = json_encode($results);
+
+    $response->getBody()->write($payload);
+    return $response
+              ->withHeader('Content-Type', 'application/json');
+    });
+
+
+$app->delete('/movil/{id}', function ($request, $response, $args) {
+
+    $id = $args['id'];
+
+    $db = SQL::connect();
+    $model = new Movil();
+
+    $results = $model->delete($db, $id);
+    SQL::close($db);
+
+    $payload = json_encode($results);
+
+    $response->getBody()->write($payload);
+    return $response
+              ->withHeader('Content-Type', 'application/json');
+    });
+
+$app->put('/movil', function ($request, $response, $args) {
+        //$token = G::Autenticar($request, "ADMIN_MODIFICAR");
+    
+        $db = SQL::connect();
+        $model = new Movil();
+    
+        $results = $model->put($db);
+
+        SQL::close($db);
+
+        $payload = json_encode($results);
+    
+        $response->getBody()->write($payload);
+        return $response
+                  ->withHeader('Content-Type', 'application/json');
+});
+
+$app->post('/movil', function ($request, $response, $args) {
+    //$token = G::Autenticar($request, "ADMIN_MODIFICAR");
+
+    $db = SQL::connect();
+    $model = new Movil();
+
+    $results = $model->post($db);
+
+    SQL::close($db);
+
+    $payload = json_encode($results);
+
+    $response->getBody()->write($payload);
+    return $response
+              ->withHeader('Content-Type', 'application/json');
+});
+
+?>
